a092e5c81ac4f75285acad9cd33ede2b0a08d5c6
antoniom57 <antoniojimenez77@aragon.unam.mx>
2022-03-01 12:51:04 -0600

    sesion 1 de marzo, automovil

--- clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
+++ clases/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
@@ -3,11 +3,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package conceptosbasicos;
-
+import ico.fes.Silla;
+import ico.fes.Automovil;
 /**
  *
  * @author antonio
  */
+import java.awt.Color;
 public class ConceptosBasicos {
 
     /**
@@ -16,6 +18,25 @@ public class ConceptosBasicos {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola de nuevo");
+        Silla s1 = new Silla();
+        System.out.println(s1);
+        System.out.println(s1.toString());
+        
+        Automovil miBocho = new Automovil();
+        System.out.println(miBocho);
+        miBocho.setMarca("VW");
+        miBocho.setSubMarca("Sedan");
+        miBocho.setModelo(1970);
+        miBocho.setColor(Color.BLUE);
+        System.out.println(miBocho);
+        
+        Automovil miMustang = new Automovil("Ford", "Mustang", 2010, Color.yellow);
+        System.out.println(miMustang);
+        
+        Automovil miAkura = new Automovil("Akura", "NSX", 2013, Color.GRAY);
+        System.out.println(miAkura);
+                
+        
     }
     
 }
