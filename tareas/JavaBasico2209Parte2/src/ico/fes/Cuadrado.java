f815ee0d7b463fc01883d70d5ee66bb7a3190d3d
antoniom57 <antoniojimenez77@aragon.unam.mx>
2022-04-28 12:59:05 -0500

    clase 28 de abril

---
+++ tareas/JavaBasico2209Parte2/src/ico/fes/Cuadrado.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ico.fes;
+
+/**
+ *
+ * @author antonio
+ */
+public class Cuadrado implements Figura {
+
+    @Override
+    public float calcularArea() {
+        System.out.println("Multiplicando lado por lado");
+        return 1.0f;
+    }
+    
+}
---
+++ tareas/JavaBasico2209Parte2/src/javabasico2209parte2/JavaBasico2209Parte2.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javabasico2209parte2;
+
+import ico.fes.Aritmetica;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author antonio
+ */
+public class JavaBasico2209Parte2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        ArrayList<String> nombres = new ArrayList<String>();
+        nombres.add("Alma");
+        nombres.add("Bartolo");
+        nombres.add("Carlos");
+        nombres.add("Diana");
+        nombres.add("Ernesto");
+        
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+            
+        }
+        
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------------");
+        Scanner teclado = new Scanner(System.in);
+        int seleccion=0;
+
+        String elNombre="";
+        
+        try {
+            seleccion = teclado.nextInt();
+            elNombre=nombres.get(seleccion);
+        } catch(InputMismatchException ie){
+            System.out.println("Debes teclear valores numericos");
+        }catch(IndexOutOfBoundsException ioe){
+            System.out.println("Error,debe ser entre 0 y 4");
+            elNombre=nombres.get(0);
+        }catch (Exception e) {
+            System.out.println("Generica");
+            elNombre=nombres.get(1);
+        }finally{
+            System.out.println(elNombre);
+            elNombre=null;
+            //limpieza
+            
+        }
+        //System.out.println(nombres.get(seleccion));
+        
+        System.out.println("Continua el programa...");
+        
+        
+        Aritmetica cal=new Aritmetica(2, 0);
+        System.out.println(cal.getA() + "+" + cal.getB() + "=" + cal.sumar() );
+        
+        
+        try {
+            System.out.println(cal.getA() + "/" + cal.getB() + "=" + cal.dividir() );
+        } catch (Exception e) {
+            System.out.println("Error de artimetica");
+        }
+        
+        
+        
+        
+        System.out.println("Fin del programa");
+        
+        //nunca permitir que un prgrama termine de forma abruptua por un error
+        
+    }
+    
+}
