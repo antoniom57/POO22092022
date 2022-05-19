f1f0af790e7adc7ec373f957a84cedc3c90625ba
antoniom57 <antoniojimenez77@aragon.unam.mx>
2022-05-19 12:59:10 -0500

    clase 19 de mayo

---
+++ clases/JavaBasico2209Parte2/src/ico/fes/modelo/ModeloPersonaCombo.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ico.fes.modelo;
+
+import ico.fes.herencia.Persona;
+import java.util.ArrayList;
+import javax.swing.ComboBoxModel;
+import javax.swing.event.ListDataListener;
+
+/**
+ *
+ * @author antonio
+ */
+public class ModeloPersonaCombo implements ComboBoxModel<Persona>{
+    private ArrayList<Persona> datos;
+    private Persona selected;
+
+    public ModeloPersonaCombo() {
+    }
+
+    public ModeloPersonaCombo(ArrayList datos, Persona selected) {
+        this.datos = datos;
+        this.selected = selected;
+    }
+
+    public ArrayList getDatos() {
+        return datos;
+    }
+
+    public void setDatos(ArrayList datos) {
+        this.datos = datos;
+    }
+
+    @Override
+    public void setSelectedItem(Object o) {
+        this.selected=(Persona)o;
+    }
+
+    @Override
+    public Object getSelectedItem() {
+        return selected;
+    }
+
+    @Override
+    public int getSize() {
+        return datos.size();
+    }
+
+    @Override
+    public Persona getElementAt(int i) {
+        return datos.get(i);
+        
+    }
+
+    @Override
+    public void addListDataListener(ListDataListener ll) {
+    }
+
+    @Override
+    public void removeListDataListener(ListDataListener ll) {
+    }
+    
+    
+    public void consultarBaseDatos(){
+        // simular una consulta a una bd
+        datos = new ArrayList<Persona>();
+        //conextiarn a bd
+        //consulta SQL
+        datos.add(new Persona("Jose", 19));
+        datos.add(new Persona("Maria", 21));
+        datos.add(new Persona("Jesus", 33));
+        datos.add(new Persona("Diana", 22));
+    }
+    
+    
+}
--- clases/JavaBasico2209Parte2/src/ico/fes/swing/VentanaSwing.java
+++ clases/JavaBasico2209Parte2/src/ico/fes/swing/VentanaSwing.java
@@ -4,19 +4,25 @@
  */
 package ico.fes.swing;
 
+import ico.fes.herencia.Persona;
+import ico.fes.modelo.ModeloPersonaCombo;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.HeadlessException;
+import java.awt.event.ItemEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 /**
@@ -28,6 +34,9 @@ public class VentanaSwing extends JFrame{
     private JTextField cuadro;
     private JButton boton;
     private JLabel resultado;
+    private JComboBox<Persona> lista;
+    private ModeloPersonaCombo modelo;
+    private JTextArea texto;
     
     public VentanaSwing() throws HeadlessException {
         setTitle("Conversion de °C a °F");
@@ -41,9 +50,29 @@ public class VentanaSwing extends JFrame{
         boton.setOpaque(true);
         boton.setToolTipText("Cick para convertir en °F");
         resultado = new JLabel("°F");
+        
+        lista = new JComboBox();
+        texto= new JTextArea(5 , 20);
+       
+        /*lista.addItem("Ingenieria");
+        lista.addItem("Arquitectura");
+        lista.addItem("Derecho");
+        lista.addItem("Periodismo");*/
+        
+        
+        modelo = new ModeloPersonaCombo();
+        modelo.consultarBaseDatos();
+        lista.setModel(modelo);
+        
+        
+        
+        
+        
         this.getContentPane().add(cuadro);
         this.getContentPane().add(boton);
         this.getContentPane().add(resultado);
+        this.getContentPane().add(lista);
+        this.getContentPane().add(texto);
         this.validate();
         this.setVisible(true);
                 
@@ -55,6 +84,20 @@ public class VentanaSwing extends JFrame{
               }
                     
                 });
+                
+                
+                this.lista.addItemListener(new ItemAdapter() {
+            @Override
+            public void itemStateChanged(ItemEvent ie) {
+                System.out.println("Evento..." + ie.getItem() );
+                texto.setText(ie.getItem() + "\n");
+             }
+                    
+                    
+                });
+                
+                
+                
                 this.boton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
